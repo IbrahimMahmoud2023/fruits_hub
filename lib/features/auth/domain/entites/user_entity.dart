5775b1b3c895df82ef74aa94e216d98e0f8f86e0
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-10 15:40:15 +0300

    create user entities and auth repo

---
+++ lib/features/auth/domain/entites/user_entity.dart
@@ -0,0 +1,9 @@
+class UserEntity {
+  final String name;
+  final String email;
+  final String password;
+  final int uId;
+
+
+  UserEntity({required this.name, required this.email, required this.password, required this.uId});
+}
