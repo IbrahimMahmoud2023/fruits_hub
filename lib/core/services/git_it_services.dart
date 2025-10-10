883f8898f2f9ef56f8e441d1cb52fc2ecab4ae89
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-10 21:53:55 +0300

    add git it services to our app

---
+++ lib/core/services/git_it_services.dart
@@ -0,0 +1,14 @@
+import 'package:fruits_ecommerec/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:fruits_ecommerec/features/auth/domain/repo/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+import 'firebase_auth_services.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthServices: getIt<FirebaseAuthServices>()),
+  );
+}
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_ecommerec/core/services/git_it_services.dart';
+import 'package:fruits_ecommerec/features/auth/domain/repo/auth_repo.dart';
+import 'package:fruits_ecommerec/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:fruits_ecommerec/features/auth/presentation/views/widgets/signup_view_body.dart';
 
 import '../../../../core/widgets/custom_app_bar.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
+
   static const String routeName = 'signup_view';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: SignupViewBody(),
+      ),
     );
   }
 }
