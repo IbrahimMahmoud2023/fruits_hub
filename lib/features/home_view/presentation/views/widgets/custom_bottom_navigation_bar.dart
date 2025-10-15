ff980373bbbc02bfa71caa5918849cb3b6f0e523
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-16 01:49:15 +0300

    add custom_bottom_navigation_bar

---
+++ lib/features/home_view/presentation/views/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 375,
+      height: 90,
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        shadows: [
+          BoxShadow(
+            color: Color(0x19000000),
+            blurRadius: 25,
+            offset: Offset(0, -2),
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+    );
+  }
+}
